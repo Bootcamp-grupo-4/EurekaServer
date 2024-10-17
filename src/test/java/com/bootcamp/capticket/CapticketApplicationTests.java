39167ff322597baf4ce7fc7449f982772b54cdf1
Dani Martinez <daniel.martinez-perez@capgemini.com>
2024-10-17 15:45:28 +0200

    Initial Commit

---
+++ src/test/java/com/bootcamp/capticket/CapticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.capticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
